c058d17526eeb1a7af7d0c9620c0777c9850e024
yhorio1111 <yameteyar__@softbank.ne.jp>
2023-08-02 01:25:39 +0900

    add dependency management plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,12 @@ repositories {
 
 val kotlinVersion: String by project
 val detektPluginVersion: String by project
+val dependencyManagementPluginVersion: String by project
 
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 	implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:$detektPluginVersion")
+
+//	spring
+	implementation("io.spring.gradle:dependency-management-plugin:$dependencyManagementPluginVersion")
 }
---
+++ buildSrc/src/main/kotlin/tabireco.spring-boot-base.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("tabireco.base")
+    kotlin("jvm")
+    id("io.gitlab.arturbosch.detekt")
+}
+
+val jvmVersion = "17"
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec)
+                .languageVersion.set(JavaLanguageVersion.of(jvmVersion))
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+//    detekt
+    val detektVersion = detekt.toolVersion
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = jvmVersion
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    config = files("$rootDir/config/detekt.yml")
+}
