463b848e9ca695e1dca7925d78fae8ac0f4e4e63
yhorio1111 <yameteyar__@softbank.ne.jp>
2023-08-02 01:27:52 +0900

    add spring-boot-starter-web

--- buildSrc/src/main/kotlin/tabireco.spring-boot-base.gradle.kts
+++ buildSrc/src/main/kotlin/tabireco.spring-boot-base.gradle.kts
@@ -1,40 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    id("tabireco.base")
-    kotlin("jvm")
-    id("io.gitlab.arturbosch.detekt")
-}
-
-val jvmVersion = "17"
-
-kotlin {
-    jvmToolchain {
-        (this as JavaToolchainSpec)
-                .languageVersion.set(JavaLanguageVersion.of(jvmVersion))
-    }
+    id("tabireco.kotlin")
+    id("io.spring.dependency-management")
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-//    detekt
-    val detektVersion = detekt.toolVersion
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = jvmVersion
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-detekt {
-    buildUponDefaultConfig = true
-    config = files("$rootDir/config/detekt.yml")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
