ecbdd69a21c1b5949240fdd8fb249c178482bf83
yhorio1111 <yameteyar__@softbank.ne.jp>
2023-08-08 01:22:05 +0900

    install spring-boot

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,12 +9,16 @@ repositories {
 
 val kotlinVersion: String by project
 val detektPluginVersion: String by project
+val springBootGradlePluginVersion: String by project
 val dependencyManagementPluginVersion: String by project
 
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+	implementation("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
+
 	implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:$detektPluginVersion")
 
 //	spring
+	implementation("org.springframework.boot:spring-boot-gradle-plugin:$springBootGradlePluginVersion")
 	implementation("io.spring.gradle:dependency-management-plugin:$dependencyManagementPluginVersion")
 }
