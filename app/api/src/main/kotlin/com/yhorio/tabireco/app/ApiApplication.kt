c1ef561b6fb92cd29938f1e44a7c55af92388d47
yhorio1111 <yameteyar__@softbank.ne.jp>
2023-08-13 17:01:25 +0900

    rename class

--- app/api/src/main/kotlin/com/yhorio/tabireco/app/ApiApplication.kt
+++ app/api/src/main/kotlin/com/yhorio/tabireco/app/ApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AppApplication
+class ApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<AppApplication>(*args)
+    runApplication<ApiApplication>(*args)
 }
