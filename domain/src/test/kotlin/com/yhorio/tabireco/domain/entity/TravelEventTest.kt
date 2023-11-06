bffcfa9d359bc996a4338f682f64e0968912b4ef
yhorio1111 <yameteyar__@softbank.ne.jp>
2023-11-07 00:55:40 +0900

    add test

--- domain/src/test/kotlin/com/yhorio/tabireco/domain/entity/TravelEventTest.kt
+++ domain/src/test/kotlin/com/yhorio/tabireco/domain/entity/TravelEventTest.kt
@@ -1,8 +1,55 @@
 package com.yhorio.tabireco.domain.entity
 
-internal class TravelEventTest {
-    @Test
-    fun ofTest() {
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.yhorio.tabireco.domain.type.TravelEventType
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.time.LocalDateTime
+import java.util.stream.Stream
 
+internal class TravelEventTest {
+    @ParameterizedTest
+    @ArgumentsSource(TravelEventTestCase::class)
+    fun isValid(
+        travelEventType: TravelEventType,
+        eventTime: LocalDateTime,
+        details: String,
+    ) {
+        val travelEvent = TravelEvent.of(
+            travelEventType,
+            eventTime,
+            details,
+        )
+
+        assertThat(travelEvent.isValid()).isEqualTo(true)
+    }
+
+    private class TravelEventTestCase: ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.arguments(
+                TravelEventType.FOOD,
+                LocalDateTime.of(2023, 9, 18, 13, 0, 0, 0),
+                "食べ物テスト"
+            ),
+            Arguments.arguments(
+                TravelEventType.ONSEN,
+                LocalDateTime.of(2023, 9, 19, 13, 0, 0 , 0),
+                "温泉テスト"
+            ),
+            Arguments.arguments(
+                TravelEventType.SIGHTSEEING,
+                LocalDateTime.of(2023, 9, 19, 13, 0, 0 , 0),
+                "観光テスト"
+            ),
+            Arguments.arguments(
+                TravelEventType.OTHERS,
+                LocalDateTime.of(2023, 9, 19, 13, 0, 0 , 0),
+                "その他テスト"
+            ),
+        )
     }
 }
--- domain/src/test/kotlin/com/yhorio/tabireco/domain/entity/TravelOverviewTest.kt
+++ domain/src/test/kotlin/com/yhorio/tabireco/domain/entity/TravelOverviewTest.kt
@@ -1,5 +1,52 @@
 package com.yhorio.tabireco.domain.entity
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.yhorio.tabireco.domain.vo.code.JpPrefectureCode
+import com.yhorio.tabireco.domain.vo.id.TravelEventId
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
-class TravelOverviewTest
+internal class TravelOverviewTest {
+    @ParameterizedTest
+    @ArgumentsSource(TravelOverviewTestCase::class)
+    fun isValid(
+        travelEventIds: List<TravelEventId>,
+        destination: String,
+        prefectures: List<JpPrefectureCode>
+    ) {
+        val travelOverview = TravelOverview.of(
+            travelEventIds,
+            destination,
+            prefectures
+        )
+
+        assertThat(travelOverview.isValid()).isEqualTo(true)
+    }
+
+    private class TravelOverviewTestCase: ArgumentsProvider {
+        val DUMMY_TRAVEL_EVENT_ID_1 = "01234567890123456789abcdef"
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.arguments(
+                listOf(DUMMY_TRAVEL_EVENT_ID_1),
+                "草津温泉",
+                listOf(JpPrefectureCode.GUNMA)
+            ),
+            Arguments.arguments(
+                emptyList<TravelEventId>(),
+                "どっか",
+                emptyList<JpPrefectureCode>()
+            ),
+            Arguments.arguments(
+                emptyList<TravelEventId>(),
+                "温泉",
+                listOf(JpPrefectureCode.TOYAMA, JpPrefectureCode.ISHIKAWA, JpPrefectureCode.FUKUI)
+            )
+        )
+    }
+}
